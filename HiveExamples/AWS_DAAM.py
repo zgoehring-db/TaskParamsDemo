7e5acb8848adc929e915d50abeec8f378cfa619c
rchynoweth <rchynoweth>
2021-10-05 20:50:29 +0000

    Finishing AWS access notebook

--- HiveExamples/AWS_DAAM.py
+++ HiveExamples/AWS_DAAM.py
@@ -1,19 +1,19 @@
 # Databricks notebook source
 # MAGIC %md
-# MAGIC # Data Access and Management - AWS (UNDER DEVELOPMENT)
+# MAGIC # Data Access and Management - AWS 
 # MAGIC 
-# MAGIC In this notebook we will walk through one of the most popular (potentially a universal best practice) for accessing data in Azure Data Lake Storage Gen2. This notebook hopes to show the controls that should be put in place for companies that require a sophisticated data governance model. Not all organizations require this level of data management as some companies may have general data requirements that apply to the majority of users, while other orgs may have various levels of access for each individual users. This notebook applies to the more complex data access requirements or wishes to have. 
+# MAGIC In this notebook we will walk through one of the most popular (potentially a universal best practice) for accessing data in [S3](https://aws.amazon.com/s3/). This notebook hopes to show the controls that should be put in place for companies that require a sophisticated data governance model. Not all organizations require this level of data management as some companies may have general data requirements that apply to the majority of users, while other orgs may have various levels of access for each individual users. This notebook applies to the more complex data access requirements. As an example alternative, some customers are completly fine using Databricks mounts even though all users will have the same data access.  
 # MAGIC 
-# MAGIC As an example, some customers are completly fine using Databricks mounts even though all users will have the same data access.  
-# MAGIC 
-# MAGIC We will complete the following items. 
-# MAGIC 1. S3 Access  
-# MAGIC 1. Create a Database with Location  
-# MAGIC 1. Create a Delta table
-# MAGIC 1. Manage access to the delta table
+# MAGIC We will complete the following items (not necessarily in this order). 
+# MAGIC - S3 Access  Overview
+# MAGIC - Create a Database with and without Location
+# MAGIC   - This is a best practice and allows for a better experience when managing tables
+# MAGIC - Create a Delta table
+# MAGIC   - Managed tables (with location) will automatically register in the database
+# MAGIC - Manage access to the delta table
 # MAGIC   - READ, WRITE, Row Level Security, and Column Level Security 
-# MAGIC 1. Show how to enforce secret scope ACLs  
-# MAGIC 1. Show how to enforce cluster ACLs  
+# MAGIC - Show how to enforce secret scope ACLs  
+# MAGIC - Show how to enforce cluster ACLs  
 # MAGIC 
 # MAGIC 
 # MAGIC 
@@ -33,8 +33,9 @@
 # MAGIC   - Lowest requried priviledge level means that if all DBSQL users only need READ access then the account should be set to that. But if some users need manage or write access then the principle will need that level of access.  
 # MAGIC   
 # MAGIC   
-# MAGIC 
-# MAGIC [Access S3 Directly](https://docs.databricks.com/data/data-sources/aws/amazon-s3.html#access-s3-buckets-directly) using an instance profile
+# MAGIC Resources:
+# MAGIC - [Access S3 Directly](https://docs.databricks.com/administration-guide/cloud-configurations/aws/instance-profiles.html) using an instance profile
+# MAGIC - Use [Field Eng](https://e2-demo-field-eng.cloud.databricks.com/) workspace.  
 
 # COMMAND ----------
 
@@ -42,89 +43,29 @@ dbutils.widgets.text("DatabaseName", "")
 database_name = dbutils.widgets.get("DatabaseName")
 dbutils.widgets.text("Directory", "ryan.chynoweth@databricks.com")
 directory = dbutils.widgets.get("Directory")
+storage_name = "oetrta"
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ### Secret Scope ACLs
+# MAGIC ### Cluster Configuration 
 # MAGIC 
-# MAGIC In the next cell we use a Databricks scope to securely store and use sensitive information or environment variables. To create a Secret Scope and add secrets to the scope users need to use the [Databricks REST API](https://docs.databricks.com/dev-tools/api/latest/secrets.html#secrets-api) or the [Databricks CLI](https://docs.databricks.com/dev-tools/cli/index.html). In this example I will use the Databricks CLI. I will assume that the configuration of the CLI has already occurred using a PAT.     
+# MAGIC I am using cluster configurations for S3 authentication using an instance profile. See the image below. Because of the instance profile the below command works. 
 # MAGIC 
-# MAGIC 1. Create a secret scope.  
-# MAGIC   ```
-# MAGIC   databricks secrets create-scope --scope demo
-# MAGIC   ``` 
-# MAGIC 
-# MAGIC 1. Add a secret to the scope.  
-# MAGIC   ```
-# MAGIC   databricks secrets put --scope demo --key oneenvstoragename --string-value <STORAGE ACCOUNT NAME>
-# MAGIC   ```
+# MAGIC <img src="https://racadlsgen2.blob.core.windows.net/public/InstanceProfile.png" width=400/>
 # MAGIC   
-# MAGIC 1. Grant a user/group the ability to READ and LIST secrets from the scope. Possible permissions are READ, WRITE, MANAGE  
-# MAGIC   ```
-# MAGIC   databricks secrets put-acl --scope demo --principal <user i.e. ryan.chynoweth@databricks.com OR group> --permission READ
-# MAGIC   ```
-# MAGIC   
-# MAGIC   It is important to note that Databricks admins will have MANAGE permissions for all secret scopes in the workspace. So non-admin users will not have access to the scope you create unless explicitly granted. In the scenario above I would have created a scope and added a secret, then gave someone permission to READ the scope. Since we are using service principals to authenticate against ADLS Gen2, we want to ensure that only specific people have access to the credentials. It would be a best practice to use groups to manage these ACLs.  
-# MAGIC   
-# MAGIC #### Summary
 # MAGIC 
-# MAGIC 1. Use secret scopes and manage permissions to the scopes at a group level.  
-# MAGIC 1. Use cluster policies and cluster ACLs to restrict which users can connect to which clusters.  
-# MAGIC 1. Ideally 1 and 2 would be managed using groups where all users would have same access to secrets and clusters. 
-# MAGIC 1. Allow cluster level or session level configuration of authentication to S3
-
-# COMMAND ----------
-
-storage_account_name = dbutils.secrets.get("rac_scope", "oneenvstoragename")
-container_name = dbutils.secrets.get("rac_scope", "oneenvcontainer")
-client_id = dbutils.secrets.get("rac_scope", "oneenvclientid")
-client_secret = dbutils.secrets.get("rac_scope", "oneenvclientsecret")
-tenant_id = dbutils.secrets.get("rac_scope", "oneenvtenantid")
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC ### Cluster Configuration - MUST FIX THE CONFIGS HERE. THEY ARE FOR ADLS NOT S3
-# MAGIC 
-# MAGIC I am using cluster configurations for my S3 authentication.  
-# MAGIC 
-# MAGIC In the Spark configuration of the cluster configuration you can input the following: 
-# MAGIC ```
-# MAGIC fs.azure.account.auth.type OAuth
-# MAGIC fs.azure.account.oauth.provider.type org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider
-# MAGIC fs.azure.account.oauth2.client.id {{secrets/rac_scope/oneenvclientid}}
-# MAGIC fs.azure.account.oauth2.client.secret {{secrets/rac_scope/oneenvclientsecret}}
-# MAGIC fs.azure.account.oauth2.client.endpoint https://login.microsoftonline.com/9f37a392-f0ae-4280-9796-f1864a10effc/oauth2/token
-# MAGIC ```
-# MAGIC 
-# MAGIC Where `{{secrets/rac_scope/oneenvclientid}}` is our ability to reference secrets stored in scopes.  
-# MAGIC 
-# MAGIC When we do this then all people who can connect to the cluster have access to these credentials. This is where [cluster ACLs](https://docs.databricks.com/security/access-control/cluster-acl.html) come into play. Here we have the ability to set up different clusters with various. In union with cluster policies you can enforce that these Spark configurations are set in a particular way so that users still have the ability to create their own clusters. 
-# MAGIC 
-# MAGIC As with Secret ACLs, admins should utilize groups as much as possible when managing access to the various resources.  
-# MAGIC 
-# MAGIC In my opinion setting these at the cluster level is a better as it is a better experience for the developer to not have to set configurations each time. Additionally, there is some limitations to the session level configs that I am not fully aware of, but one being `CREATE DATABASE <database> LOCATION '/path/database'` where this does not work if you provide a location for the database.  
-# MAGIC 
-# MAGIC Session Level Authentication is done with the following Python/Scala:  
-# MAGIC ```
-# MAGIC spark.conf.set("fs.azure.account.auth.type.{}.dfs.core.windows.net".format(storage_account_name), "OAuth")
-# MAGIC spark.conf.set("fs.azure.account.oauth.provider.type.{}.dfs.core.windows.net".format(storage_account_name), "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider")
-# MAGIC spark.conf.set("fs.azure.account.oauth2.client.id.{}.dfs.core.windows.net".format(storage_account_name), client_id)
-# MAGIC spark.conf.set("fs.azure.account.oauth2.client.secret.{}.dfs.core.windows.net".format(storage_account_name), client_secret )
-# MAGIC spark.conf.set("fs.azure.account.oauth2.client.endpoint.{}.dfs.core.windows.net".format(storage_account_name),  "https://login.microsoftonline.com/{}/oauth2/token".format(tenant_id))
-# MAGIC ```
+# MAGIC When we do this then all people who can connect to the cluster have access to these credentials. This is where [cluster ACLs](https://docs.databricks.com/security/access-control/cluster-acl.html) come into play. Here we have the ability to set up different clusters with various levels of data access. 
 
 # COMMAND ----------
 
 # list files
-dbutils.fs.ls("abfss://{}@{}.dfs.core.windows.net/".format(container_name, storage_account_name))
+dbutils.fs.ls("s3a://{}/".format(storage_name))
 
 # COMMAND ----------
 
 # DBTITLE 1,Create Directory for new Database
-# FIX THE DATA PATH!
-data_path = "abfss://{}@{}.dfs.core.windows.net/{}/databases/{}".format(container_name, storage_account_name, directory, database_name)
+data_path = "s3a://{}/{}/{}".format(storage_name, directory, database_name)
 
 
 reset_dir = False
@@ -154,6 +95,22 @@ dbutils.fs.ls(data_path+"/test_table")
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ### Table Access Control 
+# MAGIC 
+# MAGIC With [Table Access Control](https://docs.databricks.com/security/access-control/table-acls/table-acl.html) and cluster ACLs you can restrict users to only using the tables within the hive metastore. With table access you should set cluster ACLs so that users must use a cluster with table access enabled appropriately. 
+# MAGIC 
+# MAGIC Direct access described here can be used with table access. 
+# MAGIC 
+# MAGIC Please review permission levels [here](https://docs.databricks.com/security/access-control/table-acls/object-privileges.html#privileges.
+
+# COMMAND ----------
+
+# DBTITLE 1,Grant read access to the table
+# spark.sql("""GRANT SELECT ON TABLE test_table TO `TestDataScienceGroup` """) ## MUST HAVE TABLE ACCESS ENABLED ON THE CLUSTER OTHERWISE THIS ERRORS OUT. Keeping here for demo purposes
+
+# COMMAND ----------
+
 # DBTITLE 1,Must drop table and delete dir because it is an UNMANAGED table
 spark.sql("drop table if exists test_table")
 dbutils.fs.rm(data_path+"/test_table", True)
@@ -165,9 +122,6 @@ spark.sql("""CREATE DATABASE IF NOT EXISTS {}
              LOCATION '{}' """.format(database_name, data_path)
 )
 
-## 
-## this does not work on standard or hc with notebook configs
-## this does work for standard and hc using cluster configs
 
 # COMMAND ----------
 
@@ -197,9 +151,70 @@ dbutils.fs.ls(data_path)
 
 display(
 spark.sql("""
-SELECT * FROM rac_universal_db.test_table
-""")
+SELECT * FROM {}.test_table
+""".format(database_name))
 )
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Row and column level security 
+# MAGIC 
+# MAGIC This level of security can be accomplished by using [dynamic view functions](https://docs.databricks.com/security/access-control/table-acls/object-privileges.html#dynamic-view-functions). This allows us to create views with specific columns or filters to hide data dynamically using `current_user()` and `is_member()` functions.  
+# MAGIC 
+# MAGIC Below is an example where I fully restrict users from seeing a number of columns by not including them in the view but then I can dynamically show values based on their group assignment.  
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC 
+# MAGIC CREATE OR REPLACE VIEW test_view AS 
+# MAGIC 
+# MAGIC SELECT device_type, device_id, predicted_device_operational_status, -- only select columns users can see
+# MAGIC CASE WHEN is_member('TestDataScienceGroup') THEN reading_1 ELSE 'Redacted' END AS reading_1 -- use a case statement to dynamically show a column
+# MAGIC 
+# MAGIC FROM test_table
+# MAGIC WHERE is_member('TestDataScienceGroup')
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM test_view
+
+# COMMAND ----------
+
+# DBTITLE 1,Flip the case statement so column doesn't show
+# MAGIC %sql
+# MAGIC 
+# MAGIC CREATE OR REPLACE VIEW test_view AS 
+# MAGIC 
+# MAGIC SELECT device_type, device_id, predicted_device_operational_status, -- only select columns users can see
+# MAGIC CASE WHEN is_member('TestDataScienceGroup') THEN 'Redacted' ELSE reading_1 END AS reading_1 -- use a case statement to dynamically show a column
+# MAGIC 
+# MAGIC FROM test_table
+# MAGIC WHERE is_member('TestDataScienceGroup')
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM test_view
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC 
+# MAGIC CREATE OR REPLACE VIEW test_view AS 
+# MAGIC 
+# MAGIC SELECT device_type, device_id, predicted_device_operational_status, -- only select columns users can see
+# MAGIC CASE WHEN is_member('TestDataScienceGroup') THEN reading_1 ELSE 'Redacted' END AS reading_1 -- use a case statement to dynamically show a column
+# MAGIC 
+# MAGIC FROM test_table
+# MAGIC WHERE NOT is_member('TestDataScienceGroup')
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM test_view -- should see no data since I am a member of the TestDataScienceGroup 
 
 # COMMAND ----------
 
@@ -207,5 +222,9 @@ spark.sql("DROP TABLE IF EXISTS test_table")
 
 # COMMAND ----------
 
-# DBTITLE 1,Our "test_table" should be gone from the dir because it is a MANAGED table
+# DBTITLE 1,Our "test_table" should be gone from the directory because it is a MANAGED table
 dbutils.fs.ls(data_path)
+
+# COMMAND ----------
+
+
